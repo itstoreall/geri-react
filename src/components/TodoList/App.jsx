b94dece45cdce35f946cfe9eca55548819c83b7e
Serhii Stanislav <serhiistanislav@gmail.com>
2021-03-08 19:38:16 +0200

    adds a deleteTodo method

--- src/components/TodoList/App.jsx
+++ src/components/TodoList/App.jsx
@@ -1,18 +1,36 @@
 import { Component } from "react";
 import TodoList from "./TodoList";
-import todos from "./todos.json";
+import initialTodos from "./todos.json";
 import "./TodoList.scss";
 
 class App extends Component {
   state = {
-    todos: todos,
+    todos: initialTodos,
+  };
+
+  // Delete Todo
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
   };
 
   render() {
     const { todos } = this.state;
+
+    const TotalTodoCount = todos.length;
+    const completedTodoCount = todos.reduce(
+      (total, todo) => (todo.completed ? total + 1 : total),
+      0
+    );
+
     return (
       <>
-        <TodoList todos={todos} />
+        <div>
+          <p>Total: {TotalTodoCount}</p>
+          <p>Completed: {completedTodoCount}</p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
   }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,12 +3,13 @@ import "./TodoList.scss";
 import { IconButton } from "@material-ui/core";
 import DeleteTwoToneIcon from "@material-ui/icons/DeleteTwoTone";
 
-const TodoList = ({ todos }) => (
+const TodoList = ({ todos, onDeleteTodo }) => (
   <ul className="TodoList">
     {todos.map(({ id, text }) => (
       <li key={id} className="TodoList__item">
         <p className="TodoList__text">{text}</p>
         <IconButton
+          onClick={() => onDeleteTodo(id)}
           color="primary"
           aria-label="upload picture"
           component="span"
